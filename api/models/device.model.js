263b48ea33dddbbf048d9148ba5ff976e5e6e9a5
Lê Nam Khánh <55955273+khanhkhanhlele@users.noreply.github.com>
2023-12-30 02:25:16 +0700

    Change value type to string in DeviceSchema

--- api/models/device.model.js
+++ api/models/device.model.js
@@ -20,8 +20,8 @@ const DeviceSchema = mongoose.Schema({
         required: true
     },
     value: {
-        type: Number,
-        default: 0
+        type: String,
+        default: "0"
     }
 }, {
     timestamps: true
